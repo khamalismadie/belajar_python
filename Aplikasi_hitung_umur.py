cb478d7a37f706659ca1e274c54386987e865cfd
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-20 19:29:28 +0700

    master

--- Aplikasi_hitung_umur.py
+++ Aplikasi_hitung_umur.py
@@ -1,3 +1,3 @@
-Hitung_umur = input("Tahun berapa kamu lahir?")
+Hitung_umur = input("Tahun berapa kamu lahir? ")
 Umur = 2020 - int(Hitung_umur)
 print ("Usia kamu adalah: " + str(Umur) + " Tahun")

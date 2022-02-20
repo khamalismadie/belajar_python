280f36f5188f23b6d672e2e8d526b1e0b6cc968b
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-20 20:36:16 +0700

    master

---
+++ if_statements.py
@@ -0,0 +1,14 @@
+# Perintah masukan angka
+num = int(input("Masukan angkanya:"))
+# Jika angka yang di input lebih dari 10 maka:
+if num > 10:
+    # Tampilkan "Angka yang kamu masukan lebih dari 10"
+    print("Angka yang kamu masukan lebih dari 10")
+# Jika angka yang di input sama dengan 10 maka:
+elif num == 10:
+    # Tampilkan "Angka yang kamu masukan sama dengan 10"
+    print("Angka yang kamu masukan sama dengan 10")
+# Jika angka yang di input kurang dari 10 maka:
+else:
+    # Tampilkan "Angka yang kamu masukan kurang dari 10"
+    print("Angka yang kamu masukan kurang dari 10")

718b4ff1e1ff40ac2b92fdf4ac751e82ba2f934b
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-21 05:23:05 +0700

    Sqaure and Triangle

---
+++ Waktu.py
@@ -0,0 +1,8 @@
+hari = int(input("Masukan jumlah hari:"))
+jam = hari*24
+menit = jam*60
+detik = menit*60
+print("Dalam", hari, "Hari ada...")
+print(jam, "jam")
+print(menit, "menit")
+print(detik, "detik")

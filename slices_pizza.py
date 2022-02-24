2612adea651afa3d2dc883b03878698ff718480c
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-24 09:57:44 +0700

    master

---
+++ slices_pizza.py
@@ -0,0 +1,4 @@
+starNum = int(input("Enter the number of slices of pizza you started with:"))
+endNum = int(input("How many slices have you eaten?"))
+sliceleft = starNum - endNum
+print ("You have", sliceleft, "slice remaining")

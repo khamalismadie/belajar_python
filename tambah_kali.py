04185925fe555c72c4bfe22e806f2135a5fb0438
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-24 09:44:32 +0700

    master

---
+++ tambah_kali.py
@@ -0,0 +1,5 @@
+num1 = int(input("Please enter your first number:"))
+num2 = int(input("Please enter your second number:"))
+num3 = int(input("please enter your third number:"))
+answer = (num1 + num2)* num3
+print ("the answer is", answer)

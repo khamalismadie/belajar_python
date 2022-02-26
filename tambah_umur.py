8cba0fd01ba06e7af598672c68aa273407f9ffc2
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-02-26 11:17:20 +0700

    master

--- tambah_umur.py
+++ tambah_umur.py
@@ -1,4 +1,4 @@
 name = input("Siapa nama kamu?")
-age = int(input"Berapa umur kamu?")
+age = int(input("Berapa umur kamu?"))
 newAge = age + 1
-print(name, "Ulang tahunmu berikutnya kamu berumur", newAge "Tahun")
+print(name, "Ulang tahunmu berikutnya kamu berumur", newAge, "Tahun")

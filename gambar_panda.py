6a0d01d589ded124c873fa54c0a3a6cda7fcc16a
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-03-04 09:08:49 +0700

    master

--- gambar_panda.py
+++ gambar_panda.py
@@ -7,5 +7,17 @@ def ring(col,rad):
     pen.circle(rad)
     pen.end_fill()
 
+pen.up()
+pen.setpos(-35,95)
+pen.down
+ring('black', 15)
 
+pen.up()
+pen.setpos(35,95)
+pen.down
+ring('black', 15)
 
+pen.up()
+pen.setpos(0,35)
+pen.down
+ring('white', 40)
